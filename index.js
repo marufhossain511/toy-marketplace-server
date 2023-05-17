8ada9b814dde61ee4371b81f6aa6b8bf826c9621
marufhossain511 <mfmarufh@gmail.com>
2023-05-18 02:13:12 +0600

    create category api and connect with mongodb

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const app=express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port =process.env.PORT || 5000;
+require('dotenv').config()
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vvlmqn2.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const shopCollection=client.db("shopDB").collection("toys")
+
+    app.get('/getToysByCategory/:category', async(req,res)=>{
+        // console.log(req.params.category);
+        const result= await shopCollection.find({subcategory: req.params.category}).toArray()
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('toys server is running')
+})
+
+app.listen(port,()=>{
+    console.log(`toys server is running on port: ${port}`);
+})
